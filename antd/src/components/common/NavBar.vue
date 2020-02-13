06781ec6eb0f686ed16fdd9d9c5b55fadf3dbd45
1912820899 <779931346@qq.com>
2020-02-13 21:22:36 +0800

    修复文章和导航渲染没有a标签没有地址的问题

--- antd/src/components/common/NavBar.vue
+++ antd/src/components/common/NavBar.vue
@@ -7,12 +7,12 @@
           <a-sub-menu :key="item._id">
             <span slot="title" class="submenu-title-wrapper">{{item.cate_name}}</span>
             <template v-for="child in item.children">
-              <a-menu-item :key="child._id" @click="clickMenu(child)">{{child.cate_name}}</a-menu-item>
+              <a-menu-item :key="child._id" @click="clickMenu(child)"><router-link :to="'./'+child.cate_path"> {{child.cate_name}} </router-link></a-menu-item>
             </template>
           </a-sub-menu>     
         </template>
         <template v-else>
-          <a-menu-item :key="item._id" @click="clickMenu(item)">{{item.cate_name}}</a-menu-item>
+          <a-menu-item :key="item._id" @click="clickMenu(item)"><router-link :to="'./'+item.cate_path">{{item.cate_name}}</router-link></a-menu-item>
         </template>
       </template>
     </a-menu>
@@ -41,7 +41,6 @@ import bus from '../common/bus';
     },
     methods:{
       clickMenu(category){
-        this.$router.push('/'+category.cate_path);
         //点击导航菜单，动态变更title
         let title = this.$route.meta.title;
         document.title =  category.cate_path == "index" ? title : title+'-'+category.cate_name;
--- chrom.js
+++ chrom.js
@@ -2,15 +2,35 @@ const puppeteer = require('puppeteer');
 
 var fs = require("fs") ;
 
-puppeteer.launch({dumpio:true,args: ['--no-sandbox', '--disable-setuid-sandbox'],timeout: 10000}).then(
+puppeteer.launch({dumpio:true,args: ['--no-sandbox', '--disable-setuid-sandbox'],timeout: 10000}).then(async browser => {
 
-	async browser => {
-
-	fs.writeFile("chrome.txt",browser.wsEndpoint(),function (err) {
+	/* fs.writeFile("chrome.txt",browser.wsEndpoint(),function (err) {
 		
 		if (err) throw err ;
 		
 		console.log("存入chrome.txt成功"); //文件被保存
 
-    })
+	}) */
+	const page = await browser.newPage();
+	// await page.goto('http://sso.ipmph.com/login?ServiceID=examanswer2019&Referer=http%3A%2F%2Fexam.ipmph.com%2F');
+	
+	
+	try {
+        await page.goto('http://sso.ipmph.com/login?ServiceID=examanswer2019&Referer=http%3A%2F%2Fexam.ipmph.com%2F'); //到指定页面的网址.
+        await page.waitFor(500);
+    } catch (err) {
+        await page.close();
+
+        await browser.disconnect();
+
+        console.log('出现错误：'+err); // 这里捕捉到错误 `error`
+	}
+	// const html = await page.$eval('#UserName', e => e.outerHTML);
+	
+	await page.type('#UserName', 'njwx154101304'); // 立即输入
+	await page.type('#Password', 'cjx520940', {delay: 100}); // 输入变慢，像一个用户
+	// console.log(username,password);
+	let click = await page.click('#defaultLoginForm .zz-btn-a1 .zz-color input[type="button"]');
+	console.log(click);
+	
 });
